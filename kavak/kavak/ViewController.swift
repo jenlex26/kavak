74501a7dc5973f1540d17fdc6437175d898ec0e5
Javier Hernandez <javier.hernandez"qacg.com>
2020-07-17 18:54:42 -0500

    first commit

---
+++ kavak/kavak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kavak
+//
+//  Created by Javier Hernandez on 17/07/20.
+//  Copyright © 2020 Javier Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
